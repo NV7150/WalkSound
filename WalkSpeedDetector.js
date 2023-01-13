37846b1e5263f9c19f8e6589a2e517774847945c
NV7150 <7150macnv777dai@gmail.com>
2023-01-13 16:02:06 +0900

    del: debug scripts

--- WalkSpeedDetector.js
+++ WalkSpeedDetector.js
@@ -3,15 +3,6 @@ const option = {
     timeout: 1000,
     maximumAge: 0
 }
-// async function getSpeed(){
-//     let position = await new Promise(
-//         (resovle, reject) => navigator.geolocation.getCurrentPosition(resovle, reject, option)
-//     );
-//     console.log(position);
-//     return position.speed;
-// }
-//
-// function
 
 var speed = {val: 0};
 
@@ -19,14 +10,12 @@ function* getSpeed(){
     let id = navigator.geolocation.watchPosition(
         (pos) => {
             speed.val = pos.coords.speed;
-            console.log(pos.coords.speed)
-            // speed.val += 1;
+            console.log(pos.coords.speed);
             },
         () => {console.log("some error happened"); speed.val = -1; },
         option
     );
     while(true){
-        console.log(speed);
         yield speed.val;
     }
 }
