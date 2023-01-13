e3474ac35d7106bc7497907ae0077672de72648e
NV7150 <7150macnv777dai@gmail.com>
2023-01-13 15:24:02 +0900

    fix: tracing speed

--- WalkSpeedDetector.js
+++ WalkSpeedDetector.js
@@ -3,10 +3,25 @@ const option = {
     timeout: 1000,
     maximumAge: 0
 }
-async function getSpeed(){
-    let position = await new Promise(
-        (resovle, reject) => navigator.geolocation.getCurrentPosition(resovle, reject, option)
+// async function getSpeed(){
+//     let position = await new Promise(
+//         (resovle, reject) => navigator.geolocation.getCurrentPosition(resovle, reject, option)
+//     );
+//     console.log(position);
+//     return position.speed;
+// }
+//
+// function
+
+function* getSpeed(){
+    let speed = 0;
+    let id = navigator.geolocation.watchPosition(
+        (pos) => {speed = pos.coords.speed;},
+        () => {console.log("some error happened");},
+        option
     );
-    return position.speed;
+    while(true){
+        yield speed;
+    }
 }
 
